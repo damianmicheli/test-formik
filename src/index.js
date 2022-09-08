be24c05294a0ae7b256fd4ad63b5a3feb1813df8
Damian Micheli <damianmicheli@gmail.com>
2022-09-08 10:44:39 -0300

    Add form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useFormik } from "formik";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ // A custom validation function. This must return an object
+ // which keys are symmetrical to our values/initialValues
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+      errors.firstName = 'El nombre no puede estar vacio';
+    } else if (values.firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+  
+    if (!values.lastName) {
+      errors.lastName = 'El apellido no puede estar vacio';
+    } else if (values.lastName.length > 20) {
+      errors.lastName = 'Must be 20 characters or less';
+    }
+  
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+  
+    return errors;
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const SignupForm = () => {
+       // Note that we have to initialize ALL of fields with values. These
+   // could come from props, but since we don’t want to prefill this form,
+   // we just use an empty string. If we don’t do this, React will yell
+   // at us.
+  const formik = useFormik({
+    initialValues: { 
+        firstName: '',
+        lastName: '',
+        email: '',
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+    
+      <label htmlFor="firstName">Nombre</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.firstName}
+      />
+      {formik.errors.firstName && formik.touched.firstName ? <div>{formik.errors.firstName}</div> : null}
+
+
+      <label htmlFor="lastName">Apellido</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastName}
+      />
+      {formik.errors.lastName && formik.touched.lastName ? <div>{formik.errors.lastName}</div> : null}
+
+
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.errors.email && formik.touched.email ? <div>{formik.errors.email}</div> : null}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+function App() {
+  return <SignupForm />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
