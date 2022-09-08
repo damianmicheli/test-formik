39848052616a0d8e6dc32d38c72c9d6fdff7174c
Damian Micheli <damianmicheli@gmail.com>
2022-09-08 11:12:08 -0300

    Add Yup library for validations

--- src/index.js
+++ src/index.js
@@ -2,31 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useFormik } from "formik";
 import "./styles.css";
+import * as Yup from 'yup';
+
 
  // A custom validation function. This must return an object
  // which keys are symmetrical to our values/initialValues
-const validate = values => {
-    const errors = {};
-    if (!values.firstName) {
-      errors.firstName = 'El nombre no puede estar vacio';
-    } else if (values.firstName.length > 15) {
-      errors.firstName = 'Must be 15 characters or less';
-    }
+//  ESTO SE REEMPLAZA POR YUP
+// const validate = values => {
+//     const errors = {};
+//     if (!values.firstName) {
+//       errors.firstName = 'El nombre no puede estar vacio';
+//     } else if (values.firstName.length > 15) {
+//       errors.firstName = 'Must be 15 characters or less';
+//     }
   
-    if (!values.lastName) {
-      errors.lastName = 'El apellido no puede estar vacio';
-    } else if (values.lastName.length > 20) {
-      errors.lastName = 'Must be 20 characters or less';
-    }
+//     if (!values.lastName) {
+//       errors.lastName = 'El apellido no puede estar vacio';
+//     } else if (values.lastName.length > 20) {
+//       errors.lastName = 'Must be 20 characters or less';
+//     }
   
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email address';
-    }
+//     if (!values.email) {
+//       errors.email = 'Required';
+//     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//       errors.email = 'Invalid email address';
+//     }
   
-    return errors;
-  };
+//     return errors;
+//   };
+
+const validation = Yup.object({
+  firstName: Yup.string()
+    .max(15, 'Must be 15 characters or less')
+    .required('Required'),
+  lastName: Yup.string()
+    .max(20, 'Must be 20 characters or less')
+    .required('Required'),
+  email: Yup.string().email('Invalid email address').required('Required'),
+});
 
 const SignupForm = () => {
        // Note that we have to initialize ALL of fields with values. These
@@ -39,7 +52,7 @@ const SignupForm = () => {
         lastName: '',
         email: '',
     },
-    validate,
+    validationSchema: validation,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     }
